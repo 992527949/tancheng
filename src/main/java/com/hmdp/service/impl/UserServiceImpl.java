b5bfadd603040e6d4c1f068c60f80706b109aaf1
fufu <992527949@qq.com>
2025-01-12 22:37:45 +0800

    完善发送验证码

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
@@ -23,11 +26,33 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     @Override
     public Result sendCode(String phone, HttpSession session) {
         // 1 校验手机号
-
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式不正确");
+        }
         //2 不符合要求返回错误信息
         //3 符合要求，生成验证码
+        String code  = RandomUtil.randomNumbers(6);
         //4 保存验证码到session
+        session.setAttribute("code", code);
         //5 发送验证码到手机
+        log.debug("发送成功，验证码："+code);
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //1 校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式不正确");
+        }
+        //2 校验验证码
+        session.getAttribute("code");
+        //3 不一致返回错误信息
+        //4 一致，查询用户信息
+        //5 判断用户是否存在
+        //6 不存在，创建新用户
+        //7 保存用户信息到session
+        return null;
     }
 }
